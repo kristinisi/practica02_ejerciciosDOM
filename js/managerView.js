dbdba86b37e2be9274a3623f560f70bcc241292f
Cristina Gutierrez <cgutierrezestevez@gmail.com>
2024-02-08 23:59:55 +0100

    Datos creados

---
+++ js/managerView.js
@@ -0,0 +1,8 @@
+class ManagerView {
+  constructor() {
+    this.main = document.getElementsByTagName("main")[0];
+    this.categories = document.getElementById("categories");
+    this.menu = document.querySelector(".barra");
+  }
+}
+export default ManagerView;
